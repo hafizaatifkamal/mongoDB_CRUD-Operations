ca093c49b8c293ed61c86bae839b78a7a9eb952c
hafizaatifkamal <atif.k@valuebound.com>
2021-10-05 13:19:13 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+
+const client = new MongoClient(url);
+
+const dbName = "employee_db";
+
+async function main() {
+
+    await client.connect();
+
+    console.log("Connected successfully to the server");
+
+    const db = client.db(dbName);
+
+    const collection = db.collection("employee");
+
+    return "done.";
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
